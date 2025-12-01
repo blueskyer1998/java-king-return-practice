e515818f088c5df619fa157e3b261883e54c27d4
blueskyer1998 <litpm1998@gmail.com>
2025-12-01 22:53:37 +0800

    初始化專案

---
+++ src/main/java/com/blueskyer1998/App.java
@@ -0,0 +1,13 @@
+package com.blueskyer1998;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
